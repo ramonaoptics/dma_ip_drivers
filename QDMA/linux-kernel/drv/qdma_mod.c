8a639901922f9b036338528aa1266f33a90d2999
Mark Harfouche <mark.harfouche@gmail.com>
2019-05-20 20:07:59 -0400

    attemp to fixup early free of resources in xpdev_queue_delete

--- QDMA/linux-kernel/drv/qdma_mod.c
+++ QDMA/linux-kernel/drv/qdma_mod.c
@@ -1128,7 +1128,7 @@ int xpdev_queue_delete(struct xlnx_pci_dev *xpdev, unsigned int qidx, u8 q_type,
 {
 	struct xlnx_qdata *qdata = xpdev_queue_get(xpdev, qidx, q_type, 1, ebuf,
 						ebuflen);
-	int rv = 0;
+	int rv = -EINVAL;
 
 	if (!qdata)
 		return -EINVAL;
