99a101cba164d283282a26609f4a51024166ed92
Mark Harfouche <mark.harfouche@gmail.com>
2019-06-12 12:57:38 -0400

    Fixup erroneous use of sprintf and return value in qdma_cdev_create

--- QDMA/linux-kernel/drv/cdev.c
+++ QDMA/linux-kernel/drv/cdev.c
@@ -616,10 +616,8 @@ int qdma_cdev_create(struct qdma_cdev_cb *xcb, struct pci_dev *pdev,
 		pr_err("%s failed to allocate cdev %lu.\n",
 		       qconf->name, sizeof(struct qdma_cdev));
 		if (ebuf && ebuflen) {
-			rv = sprintf(ebuf, "%s failed to allocate cdev %lu.\n",
+			snprintf(ebuf, ebuflen, "%s failed to allocate cdev %lu.\n",
 				qconf->name, sizeof(struct qdma_cdev));
-			ebuf[rv] = '\0';
-
 		}
 		return -ENOMEM;
 	}
@@ -636,9 +634,8 @@ int qdma_cdev_create(struct qdma_cdev_cb *xcb, struct pci_dev *pdev,
 	if (xcdev->minor >= xcb->cdev_minor_cnt) {
 		pr_err("%s: no char dev. left.\n", qconf->name);
 		if (ebuf && ebuflen) {
-			rv = sprintf(ebuf, "%s cdev no cdev left.\n",
+			snprintf(ebuf, ebuflen, "%s cdev no cdev left.\n",
 					qconf->name);
-			ebuf[rv] = '\0';
 		}
 		rv = -ENOSPC;
 		goto err_out;
@@ -652,9 +649,8 @@ int qdma_cdev_create(struct qdma_cdev_cb *xcb, struct pci_dev *pdev,
 	if (rv < 0) {
 		pr_err("cdev_add failed %d, %s.\n", rv, xcdev->name);
 		if (ebuf && ebuflen) {
-			int l = sprintf(ebuf, "%s cdev add failed %d.\n",
+			snprintf(ebuf, ebuflen, "%s cdev add failed %d.\n",
 					qconf->name, rv);
-			ebuf[l] = '\0';
 		}
 		goto err_out;
 	}
@@ -668,10 +664,9 @@ int qdma_cdev_create(struct qdma_cdev_cb *xcb, struct pci_dev *pdev,
 			pr_err("%s device_create failed %d.\n",
 				xcdev->name, rv);
 			if (ebuf && ebuflen) {
-				int l = sprintf(ebuf,
-						"%s device_create failed %d.\n",
-						qconf->name, rv);
-				ebuf[l] = '\0';
+				snprintf(ebuf, ebuflen,
+					"%s device_create failed %d.\n",
+					qconf->name, rv);
 			}
 			goto del_cdev;
 		}
